f32afd594c2224c9d602ae06a127d00e24d1b565
Guillaume Lambert <guillaume.p.lambert@gmail.com>
2014-05-15 17:44:41 +0200

    Integrated the Sorting list x first algorithms in python

---
+++ Sorting list x first/sorting_lists_x_first.js
@@ -0,0 +1,6 @@
+Given a list of strings, return a list with the strings
+# in sorted order, except group all the strings that begin with 'x' first.
+# e.g. ['mix', 'xyz', 'apple', 'xanadu', 'aardvark'] yields
+# ['xanadu', 'xyz', 'aardvark', 'apple', 'mix']
+# Hint: this can be done by making 2 lists and sorting each of them
+# before combining them.
